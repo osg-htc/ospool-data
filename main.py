9c9e62627ff850d4db701ccb215d0b9b1fc1f389
Cannon Lock <clock@wisc.edu>
2022-11-16 15:13:28 -0600

    Add Sort

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 SUMMARY_INDEX = "daily_totals"
-ENDPOINT = "localhost:9200"
+ENDPOINT = "http://localhost:9200"
 HEADERS = {'Content-Type': 'application/json'}
 
 test_query = {
@@ -14,7 +14,10 @@ test_query = {
                 { "term" : { "report_period": "daily" }},
             ]
         }
-    }
+    },
+    "sort": [
+        {"date" : "desc"}
+    ]
 }
 
 response_json = requests.get(f"{ENDPOINT}/{SUMMARY_INDEX}/_search", data=json.dumps(test_query), headers=HEADERS).json()
