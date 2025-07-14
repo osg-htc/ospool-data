c1553e17f428e85a2e2db39867e83bf5c7fe6ad0
Cannon Lock <clock@wisc.edu>
2025-07-14 14:14:10 -0500

    Push update

--- util.py
+++ util.py
@@ -60,9 +60,8 @@ CUSTOM_MAPPING = {
 
 
 def get_schedd_collector_host_map():
+    """Get the Schedd to CollectorHost mapping for the OSPool"""
 
-    collector_host = "cm-1.ospool.osg-htc.org"
-    collector_hosts = {"cm-1.ospool.osg-htc.org", "cm-2.ospool.osg-htc.org"}
     schedd_collector_host_map_pickle = Path("ospool-host-map.pkl")
     schedd_collector_host_map = {}
     if schedd_collector_host_map_pickle.exists():
@@ -70,45 +69,13 @@ def get_schedd_collector_host_map():
             schedd_collector_host_map = pickle.load(open(schedd_collector_host_map_pickle, "rb"))
         except IOError:
             pass
+    else:
+        print("No pickle found, just using custom mappings")
+
     schedd_collector_host_map.update(CUSTOM_MAPPING)
 
-    collector = htcondor.Collector(collector_host)
-    schedds = [ad["Machine"] for ad in collector.locateAll(htcondor.DaemonTypes.Schedd)]
-
-    for schedd in schedds:
-        schedd_collector_host_map[schedd] = set()
-
-        for collector_host in collector_hosts:
-            collector = htcondor.Collector(collector_host)
-            ads = collector.query(
-                htcondor.AdTypes.Schedd,
-                constraint=f'''Machine == "{schedd.split('@')[-1]}"''',
-                projection=["Machine", "CollectorHost"],
-            )
-            ads = list(ads)
-            if len(ads) == 0:
-                continue
-            if len(ads) > 1:
-                print(f'Got multiple Schedd ClassAds for Machine == "{schedd}"')
-
-            # Cache the CollectorHost in the map
-            if "CollectorHost" in ads[0]:
-                schedd_collector_hosts = set()
-                for schedd_collector_host in ads[0]["CollectorHost"].split(","):
-                    schedd_collector_host = schedd_collector_host.strip().split(":")[0]
-                    if schedd_collector_host:
-                        schedd_collector_hosts.add(schedd_collector_host)
-                if schedd_collector_hosts:
-                    schedd_collector_host_map[schedd] = schedd_collector_hosts
-                    break
-        else:
-            print(f"Did not find Machine == {schedd} in collectors")
-
-    # Update the pickle
-    with open(schedd_collector_host_map_pickle, "wb") as f:
-        pickle.dump(schedd_collector_host_map, f)
-
     return schedd_collector_host_map
+
 
 if __name__ == "__main__":
     get_schedd_collector_host_map()
